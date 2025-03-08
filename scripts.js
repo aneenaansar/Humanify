a3da4706ff238ed9881112bd4cde7a92abd91dc7
aneena ansar <131254828+aneenaansar@users.noreply.github.com>
2025-03-08 13:04:42 +0530

    updated

--- scripts.js
+++ scripts.js
@@ -291,3 +291,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', function() {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
+});
